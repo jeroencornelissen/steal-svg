12a6b1781884851846f2c90131144cbfa788c4d2
Jeroen Cornelissen <jeroencornelissen.be@gmail.com>
2017-05-23 16:34:55 +0200

    added linebreak regex

--- steal-svg.js
+++ steal-svg.js
@@ -1,9 +1,10 @@
 const xmlTagRegex = new RegExp(/<\?xml(.*?)?>/, 'gi');
 const nsRegex = new RegExp(/xmlns(.*?)"(.*?)"/, 'gi');
+const lineBreakRegex = new RegExp(/(\r\n|\n|\r)/, 'g');
 
 exports.translate = function (load) {
   load.metadata.format = 'es6';
   load.source = load.source.replace(xmlTagRegex, '');
   load.source = load.source.replace(nsRegex, '');
-  load.source = `export default \`${load.source}\`;`;
+  load.source = 'export default \'' + load.source + '\';';
 };
